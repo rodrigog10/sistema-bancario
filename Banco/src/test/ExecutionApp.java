ff10bd0bbb9c815259279467566dff593ff76f4e
Jose Rodrigo Bispo de Oliveira e Silva <jose.bispo@grupobrasanitas.com.br>
2026-07-31 13:58:27 -0300

    Ajustando a ligação da conta do usuário ao seus dados bancários

--- Banco/src/test/ExecutionApp.java
+++ Banco/src/test/ExecutionApp.java
@@ -12,6 +12,8 @@ public class ExecutionApp {
         App agencia = new App();
         Scanner input = new Scanner(System.in);
         boolean login = false;
+        Cliente clienteLogado = null;
+        Bradesco contaLogada = null;
         while (!login) {
             try {
                 System.out.println("Digite seu email: ");
@@ -19,6 +21,17 @@ public class ExecutionApp {
                 System.out.println("Digite sua senha: ");
                 int senhaDigitada = input.nextInt();
                 input.nextLine();
+                    for (int i = 0; i < clientes.size() && i < contas.size(); i++) {
+                        if (clientes.get(i).getEmail().equals(emailDigitado) &&  clientes.get(i).getSenha()== (senhaDigitada)) {
+                            clienteLogado = clientes.get(i);
+                            contaLogada = contas.get(i);
+                            login = true;
+                        }
+                        if (login) {
+                            System.out.println("Bem vindo, " + clientes.get(i).getNome());
+                            agencia.iniciar(clienteLogado, contaLogada);
+                        }
+                    } // AJUSTAR A LIGAÇÃO DOS DADOS
 
 
             } catch (Exception e) {
--- Banco/src/test/Main.java
+++ Banco/src/test/Main.java
@@ -12,7 +12,9 @@ public class Main {
         List<Cliente> clientes = new ArrayList<>();
         {
             clientes.add(new  Cliente("José Rodrigo", 18, "rodrigo@gmail.com", 1234));
+            //0, rodrigo
             clientes.add(new Cliente("Ana Clara", 19, "anaclara@gmail.com", 123));
+            //1, ana
         }
         List<Bradesco> contas = new ArrayList<>(); {
             contas.add(new Bradesco(10000, 15000, 3500, 5000)); //rodrigo
