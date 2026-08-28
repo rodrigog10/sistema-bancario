ec81dc71f6caa87e3f69921ba0e3fd9de0031219
Rodrigo Bispo <rodrigobisppo123@gmail.com>
2026-08-27 21:32:37 -0300

    continuando os estudos em sql..

---
+++ Banco/src/banco/connection/DataBaseConnection.java
@@ -0,0 +1,6 @@
+package banco.connection;
+
+public class DataBaseConnection {
+
+
+}
--- Banco/src/banco/connection/TesteConexao.java
+++ Banco/src/banco/connection/TesteConexao.java
@@ -1,4 +1,4 @@
-package banco;
+package banco.connection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
